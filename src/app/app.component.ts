8c42af06510b18de9fd791adda4ccb5d86bb398f
Javier Lete <javier@eskura21.com>
2019-10-02 20:07:27 +0200

    Ejemplos de Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
 import { Persona } from './persona';
 
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/es';
+
+// the second parameter 'fr' is optional
+registerLocaleData(localeFr, 'es');
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   datos = [
     { id: 1, nombre: 'Javier' },
     { id: 2, nombre: 'Pepe' },
     { id: 3, nombre: 'Juan' }
   ];
   mensaje: string;
+
+  fecha = new Date();
+
+  precio = 12.34;
 
   porCadaPersona(indice: number, persona: Persona): number {
     // console.log(indice, persona);
