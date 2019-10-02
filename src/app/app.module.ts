2f73178daa6ba491ce9e2eadc7b3313fd5a2483d
Javier Lete <javier@eskura21.com>
2019-10-02 20:33:42 +0200

    Ejemplo de paso de datos a través de set y get

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HijoComponent } from './hijo/hijo.component';
 import { HeroParentComponent } from './hero-parent.component';
 import { HeroChildComponent } from './hero-child.component';
+import { NameParentComponent } from './name-parent.component';
+import { NameChildComponent } from './name-child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HijoComponent,
     HeroParentComponent,
-    HeroChildComponent
+    HeroChildComponent,
+    NameParentComponent,
+    NameChildComponent
   ],
   imports: [
     BrowserModule,
