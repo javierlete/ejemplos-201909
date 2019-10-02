a5a6b4cb2e8b83f30c9c3da8a31053245638d0ca
Javier Lete <javier@eskura21.com>
2019-10-02 19:08:02 +0200

    Ejemplo @Input

--- src/app/hijo/hijo.component.ts
+++ src/app/hijo/hijo.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Persona } from '../persona';
 
 @Component({
   selector: 'app-hijo',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hijo.component.css']
 })
 export class HijoComponent implements OnInit {
+
+  // tslint:disable-next-line: no-input-rename
+  @Input('el-que-recibe-el-saludo') persona: Persona = { id: 0, nombre: 'Desconocido' };
 
   constructor() { }
 
